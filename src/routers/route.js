cc943ecb72a10c5647c9159ec3ac0905a1bc190b
Lucas Pedro <rodrigerioo@gmail.com>
2023-02-15 09:25:57 -0300

    requisito 04

--- src/routers/route.js
+++ src/routers/route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const productController = require('../controllers/productController');
+const validation = require('../middlewares/validations');
 
 const router = express.Router();
 
 router.get('/products', productController.allProducts);
 
 router.get('/products/:id', productController.productById);
-router.post('/products', productController.productRegistration);
+router.post('/products', validation.validProduct, productController.productRegistration);
 
 module.exports = router;
